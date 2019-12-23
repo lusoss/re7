988278149391d3582f0587eb473ab6990284ff01
Lusoss <lusosos@outlook.com>
2019-12-23 16:38:25 -0500

    format

--- src/main/java/io/luso/Main.java
+++ src/main/java/io/luso/Main.java
@@ -1,5 +1,6 @@
 package io.luso;
 
+import io.luso.domain.Recipe;
 import io.luso.ui.di.DaggerAppUiComponent;
 import javafx.application.Application;
 import javafx.scene.Scene;
--- src/test/java/io/luso/domain/RecipeTest.java
+++ src/test/java/io/luso/domain/RecipeTest.java
@@ -2,8 +2,6 @@ package io.luso.domain;
 
 import org.junit.Before;
 
-import static org.junit.Assert.*;
-
 public class RecipeTest {
 
     private Recipe recipe;
